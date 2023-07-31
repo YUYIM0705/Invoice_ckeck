7aec2993d7fce97ed0beb639de5a2a284d1ba367
YUYIM0705 <nine52440@gmail.com>
2023-07-31 15:15:03 +0800

    Add func get_max_matches_for_wim_num

--- src/invoice.py
+++ src/invoice.py
@@ -7,3 +7,25 @@ if __name__ == '__main__':
     ticket = '94899145'
     last_4_digits = get_last_n_numbers(ticket, 4)
     assert last_4_digits == '9145'
+
+def get_max_matches_for_win_num(ticket,win_num):
+    max_matches = 0
+    for i in range(3,9):
+        t = get_last_n_numbers(ticket,i)
+        w = get_last_n_numbers(win_num,i)
+        if t == w:
+            max_matches = i
+        else:
+            break
+    return max_matches
+
+def get_max_matches_for_win_num(ticket,win_num):
+    max_matches = 0
+    for i in range(3,9):
+        t = get_last_n_numbers(ticket,i)
+        w = get_last_n_numbers(win_num,i)
+        if t == w:
+            max_matches = i
+        else:
+            break
+    return max_matches
--- testcases/test_invoice.py
+++ testcases/test_invoice.py
@@ -1,4 +1,4 @@
-from src.invoice import get_last_n_numbers
+from src.invoice import get_last_n_numbers, get_max_matches_for_win_num
 
 def test_get_last_3_numbers():
     ticket = '94899145'
@@ -9,3 +9,21 @@ def test_get_last_8_numbers():
     ticket = '94899145'
     last_8_digits = get_last_n_numbers(ticket, 8)
     assert last_8_digits == '94899145'
+
+def get_3_matches_for_win_num(ticket,win_num):
+    ticket = '94899145'
+    win_num = '94898145'
+    max_matches = get_max_matches_for_win_num(ticket,win_num)
+    assert max_matches == 3
+
+def get_no_matches_for_win_num(ticket,win_num):
+    ticket = '94899145'
+    win_num = '94898045'
+    max_matches = get_max_matches_for_win_num(ticket,win_num)
+    assert max_matches == 0
+
+def get_8_matches_for_win_num(ticket,win_num):
+    ticket = '94899145'
+    win_num = '94899145'
+    max_matches = get_max_matches_for_win_num(ticket,win_num)
+    assert max_matches == 8
